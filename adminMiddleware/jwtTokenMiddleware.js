5b7495a59fd13c1d3d38b0ed88b6e36621690e42
amit04-07-1999 <ameykumar76@gmail.com>
2024-07-08 13:46:15 +0530

    replace

--- adminMiddleware/jwtTokenMiddleware.js
+++ adminMiddleware/jwtTokenMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 
 const jwtTokenMiddleware = (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
         return res.status(401).json({ error: 'Access denied' });
